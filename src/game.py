4f2b958d11268e259cb26b718c00df4639624236
Anni-Liisa <anniliisa.laaksonen@gmail.com>
2021-04-09 20:43:28 +0300

    muokkaus

--- src/game.py
+++ src/game.py
@@ -1,66 +1,12 @@
 
 import pygame
 
+from pacman import Pacman
 
-
-class Pacman:
-
-    def __init__(self, pacman: pygame.Surface, pacman_x: int, pacman_y: int):
-        self.pacman = pacman
-        self.pacman_x = pacman_x
-        self.pacman_y = pacman_y
-        self.left = False
-        self.right = False
-        self.up = False
-        self.down = False
-    
-
-    def move(self):
-        while True:
-            for event in pygame.event.get():
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_LEFT:
-                        self.left = True
-                    
-                    if event.key == pygame.K_RIGHT:
-                        self.right = True
-                    
-                    if event.key == pygame.K_UP:
-                        self.up = True
- 
-                    if event.key == pygame.K_DOWN:
-                        self.down = True
- 
-                if event.type == pygame.KEYUP: 
- 
-                    if event.key == pygame.K_LEFT:
-                        self.left = False
-                    if event.key == pygame.K_RIGHT:
-                        self.right = False
-                    if event.key == pygame.K_UP:
-                        self.up = False
-                    if event.key == pygame.K_DOWN:
-                        self.down = False
-
-                if event.type == pygame.QUIT:
-                    exit()
-                
-        if self.right:
-            self.pacman_x += 4
- 
-        if self.left:
-            self.pacman_x -= 4
- 
-        if self.up:
-            self.pacman_y -= 4
- 
-        if self.down:
-            self.pacman_y += 4
  
         
         
-
-class Game(Pacman):
+class Game:
     
     def __init__(self):
         
