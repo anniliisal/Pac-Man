71f1c2edfca5c293f96576ad2261f3be3be992b0
Anni-Liisa <anniliisa.laaksonen@gmail.com>
2021-04-09 10:47:12 +0300

    uus pacman

--- src/peli.py
+++ src/peli.py
@@ -1,59 +1,15 @@
 import pygame
 
+pygame.init()
+naytto = pygame.display.set_mode((640, 480))
 
-class Game:
-    pygame.init()
+pacman = pygame.image.load("/Users/anni-liisalaaksonen/ot-harjoitustyo/ot-harjoitustyo/src/pacman2.png")
 
-    def __init__(self):
-        self.screen = pygame.display.set_mode((640, 480))
-        self.height = 480
-        self.widht = 640
-        self.pacman = Pacman()
-        self.move = Pacman()
-        self.play()
-        
+naytto.fill((0, 0, 0))
+naytto.blit(pacman, (100, 50))
+pygame.display.flip()
 
-
-
-    def play(self):
-        self.draw_screen()
-        self.move()
-
-
-
-    def draw_screen(self):
-        self.screen.fill((0, 0, 0))
-        self.screen.blit(self.pacman,(self.pacman_x, self.pacman_y))
-
-        pygame.display.flip
-
-
-    
-
-class Pacman:
-
-    def __init__(self):
-
-        self.pacman = pygame.image.load("pacman.png")
-        self.pacman_x = 100
-        self.pacman_y = 100
-       
-
-
-
-    def move(self):
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                exit()
-
-
-
-        
-    
-
-
-
-
-if __name__=="__main__":
-    Pacman()
+while True:
+    for tapahtuma in pygame.event.get():
+        if tapahtuma.type == pygame.QUIT:
+            exit()
