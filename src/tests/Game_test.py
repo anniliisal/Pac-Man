9aab6ff38850e3f7657c56929134f44b76ac041f
Anni-Liisa <anniliisa.laaksonen@gmail.com>
2021-04-12 10:12:00 +0300

    uusi testi

---
+++ src/tests/Game_test.py
@@ -0,0 +1,12 @@
+import unittest
+from game import *
+from pacman import *
+
+class TestGame(unittest.TestCase):
+      def setUp(self):
+          print("Set up goes here")
+
+      def test_pacman_loads_pacman(self):
+          self.pacman = Pacman.load_pacman(self)
+          find = self.pacman
+          self.assertEqual(find, self.pacman)
