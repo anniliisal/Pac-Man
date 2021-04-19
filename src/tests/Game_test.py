9a2781ecc44a329aeabd13fe28c0e0697c9ed58a
Anni-Liisa <anniliisa.laaksonen@gmail.com>
2021-04-19 17:08:30 +0300

    muokkaus

--- src/tests/Game_test.py
+++ src/tests/Game_test.py
@@ -7,7 +7,17 @@ class TestGame(unittest.TestCase):
           print("Set up goes here")
      
       
-      def test_pacman_load_pacman(self):
-          self.pacman = Pacman.load_pacman(self)
-          find = self.pacman
-          self.assertEqual(find, self.pacman)
+      def test_pacman_load_pacman1(self):
+          self.pacman1 = Pacman.load_pacman1(self)
+          find = self.pacman1
+          self.assertEqual(find, self.pacman1)
+     
+      def test_pacman_load_pacman2(self):
+          self.pacman2 = Pacman.load_pacman2(self)
+          find = self.pacman2
+          self.assertEqual(find, self.pacman2)
+      
+      def test_draw_screen(self):
+          self.screen = self.screen_measures()
+          find = self.screen
+          self.assertEqual(find, self.screen)
